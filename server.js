8a2434c7ee8c6539552f8bfb59a83109f11c09a7
David Enochs <enix2direct@gmail.com>
2025-02-27 15:18:28 -0500

    Added POST route for recipes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public')); // Serve static files
-app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+app.post('/recipes', (req, res) => {
+  const recipeTitle = req.body.title; // Get the form input
+  res.send(`Added recipe: ${recipeTitle}`); // Temporary response
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
