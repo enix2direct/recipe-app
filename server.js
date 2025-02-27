e3f494d0e9c1e6021c1a7e85257da8134e97512d
David Enochs <enix2direct@gmail.com>
2025-02-27 15:09:42 -0500

    Task 4: Added frontend form

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public')); // Serve static files
+app.use(express.urlencoded({ extended: true })); // Parse form data
 app.get('/', (req, res) => {
-  res.send('Hello, Recipe App!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
